ffdf83c04d23e437a50711bbdfca5a7f00accf12
Pranit <patilpranit3112@gmail.com>
2024-01-08 14:34:27 +0530

    Socket.io Implemented Successfully✅ - Simple Chat App Completed

---
+++ public/main.js
@@ -0,0 +1,87 @@
+const socket = io();
+
+let nameInput = document.getElementById("name-input");
+let clientTotal = document.getElementById("client-total");
+let messageForm = document.getElementById("message-form");
+let messageInput = document.getElementById("message-input");
+let messageContainer = document.getElementById("message-container");
+
+const MessageNotification=new Audio('/Message-notification.mp3')
+
+const scrollToBottom = () => {
+    messageContainer.scrollTo(0, messageContainer.scrollHeight);
+};
+
+messageInput.addEventListener("focus", (e) => {
+    socket.emit("feedback", {
+        feedback: `✍️${nameInput.value} is typing a message`,
+    });
+});
+messageInput.addEventListener("keypress", (e) => {
+    socket.emit("feedback", {
+        feedback: `✍️${nameInput.value} is typing a message`,
+    });
+});
+messageInput.addEventListener("blur", (e) => {
+    socket.emit("feedback", {
+        feedback: "",
+    });
+});
+
+const sendMessage = () => {
+    if (messageInput.value === "") {
+        return;
+    }
+    const messageData = {
+        userName: nameInput.value,
+        message: messageInput.value,
+        date: new Date(),
+    };
+    socket.emit("send-message", messageData);
+    sendMessageToUI(true, messageData);
+    messageInput.value = "";
+};
+
+const sendMessageToUI = (isOwnMessage, messageData) => {
+    clearFeedback()
+    const messageElement = `<li class="${isOwnMessage ? "message-right" : "message-left"
+        }">
+          <p class="message">
+            ${messageData.message}
+            <span>${messageData.userName} ⚫ ${moment(
+            messageData.date
+        ).fromNow()} </span>
+          </p>
+        </li>`;
+
+    messageContainer.innerHTML += messageElement;
+    scrollToBottom();
+};
+
+const clearFeedback = () => {
+    document.querySelectorAll('li.message-feedback').forEach(element => {
+        element.parentElement.removeChild(element)
+    })
+}
+
+messageForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    sendMessage();
+});
+socket.on("clients-total", (data) => {
+    clientTotal.innerText = `Total Clients : ${data}`;
+});
+
+socket.on("chat-message", (messageData) => {
+    MessageNotification.play()
+    sendMessageToUI(false, messageData);
+});
+
+socket.on("feedback", (data) => {
+    clearFeedback()
+    const element = `<li class="message-feedback">
+          <p class="feedback">${data.feedback}</p>
+        </li>`;
+
+    messageContainer.innerHTML += element;
+});
--- server.js
+++ server.js
@@ -6,8 +6,35 @@ const app = express();
 
 const PORT = 3000;
 
-app.use(express.static(path.join(__dirname, "public")));
-
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server Running on Port ${PORT}`);
 });
+
+const io = require("socket.io")(server);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+const socketConnected = new Set();
+
+const onConnected = (socket) => {
+  console.log(socket.id);
+  socketConnected.add(socket.id);
+
+  io.emit("clients-total", socketConnected.size);
+
+  socket.on("disconnect", () => {
+    console.log("Socket Disconnected ", socket.id);
+    socketConnected.delete(socket.id);
+    io.emit("clients-total", socketConnected.size);
+  });
+
+  socket.on("send-message", (messageData) => {
+    socket.broadcast.emit("chat-message", messageData);
+  });
+
+  socket.on("feedback", (data) => {
+    socket.broadcast.emit("feedback", data);
+  });
+};
+
+io.on("connection", onConnected);
